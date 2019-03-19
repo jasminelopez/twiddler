dd3b9845530b3101667941e9336fa3f94cabd97b
jasminelopez <lopez.jasmine094@gmail.com>
2019-03-19 15:41:04 -0700

    Modified home button, and modified tweet layout by adding timestamp underneath tweet message

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
     //console.log(streams.users.shawndrost);
     //creating tweets for home page
     var $tweet = $('<div class="tweets"> <a href="'+ username +'.html" class="' + username + '"> ' + '@' +
-                   username  +'</a> <div id="message">' + ': ' + tweet.message + timeStamp() + '</div></div>'); 
+                   username  +'</a> <div id="message">' + ': ' + tweet.message + '<div>' + ' ' + timeStamp() + '</div></div></div>'); 
     //appending tweets to home page
     $tweet.appendTo( $( '.tweet-container' ) );
 
@@ -33,32 +33,28 @@ $(document).ready(function(){
     index -= 1;
   }
 
-//$(".shawndrost").on('click', function(elem){ //when shawn is clicked on- his tweets are appended to his homepage
-
-  //we want the same tweets from the home page to go into shawns timeline on his homepage                      
-  // var i = streams.users.shawndrost.length - 1;
-  
-  // while(i >= 0){
-  //   var userTweet = streams.users.shawndrost[i];
-  //   var timelineTweets = $('<div class="shawnTweets">' + userTweet.message  + timeStamp() + '</div>');
-  //   timelineTweets.appendTo( $( '.shawn-tweet-container' ) );
-  //   i -= 1;
-  // }
-//});
-
    $(".newtweets").on("click", function(){
      var index = streams.home.length - 1;
-      while(index >= 0){
-        var tweet = streams.home[index];
-        var $tweet = $('<div class="tweets"><div class="username"> ' + tweet.user + '</    </div>');
+  while(index >= 0){
+    var tweet = streams.home[index];
+    var username = tweet.user;
+    var $tweet = $('<div class="tweets"> <a href="'+ username +'.html" class="' + username + '"> ' + '@' +
+                   username  +'</a> <div id="message">' + ': ' + tweet.message + '<div>' + ' ' + timeStamp() + '</div></div></div>'); 
+    $tweet.appendTo( $( '.tweet-container' ) );
 
-        $tweet.text('@' + tweet.user + ': ' + tweet.message + timeStamp()); 
-        $tweet.appendTo(".tweet-container" );
-        index -= 1;
-      }  
-   });
-
-   //console.log(streams.home);
-
-
+    if (username === 'shawndrost') {
+      $tweet.appendTo( $('.shawn-tweet-container') );
+    } 
+    if (username === 'douglascalhoun') {
+      $tweet.appendTo( $('.douglas-tweet-container') );
+    }
+    if (username === 'mracus') {
+      $tweet.appendTo( $('.mracus-tweet-container') );
+    }
+    if (username === 'sharksforcheap') {
+      $tweet.appendTo( $('.sharks-tweet-container') );
+    }
+    index -= 1;
+  }
+ });
 });
