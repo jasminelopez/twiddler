bf4fbaf90167d4ce6a790ea51ad4a1d2e77b4546
jasminelopez <lopez.jasmine094@gmail.com>
2019-03-19 15:21:35 -0700

    Add each users tweets to their own timeline

--- app.js
+++ app.js
@@ -8,29 +8,43 @@ $(document).ready(function(){
   }
 
   var index = streams.home.length - 1;
-
   while(index >= 0){
     var tweet = streams.home[index];
     var username = tweet.user;
-    var $tweet = $('<div class="tweets"> <a href="'+ username +'.html" class="' + username + '"> ' + '@' + username  +'</a> <div id="message">' + ': ' + tweet.message + timeStamp() + '</div></div>');
-
+    //console.log(streams.users.shawndrost);
+    //creating tweets for home page
+    var $tweet = $('<div class="tweets"> <a href="'+ username +'.html" class="' + username + '"> ' + '@' +
+                   username  +'</a> <div id="message">' + ': ' + tweet.message + timeStamp() + '</div></div>'); 
+    //appending tweets to home page
     $tweet.appendTo( $( '.tweet-container' ) );
+
+    if (username === 'shawndrost') {
+      $tweet.appendTo( $('.shawn-tweet-container') );
+    } 
+    if (username === 'douglascalhoun') {
+      $tweet.appendTo( $('.douglas-tweet-container') );
+    }
+    if (username === 'mracus') {
+      $tweet.appendTo( $('.mracus-tweet-container') );
+    }
+    if (username === 'sharksforcheap') {
+      $tweet.appendTo( $('.sharks-tweet-container') );
+    }
     index -= 1;
   }
 
-$(".shawndrost").on('click', function(elem){
-  if (elem.target == this) {
-    var text = $(this).text(); //we are trying to access the username text                         
-  }
-    
-  var i = streams.users.shawndrost.length - 1;
-  while(i >= 0){
-    var userTweet = streams.users.shawndrost[i];
-    var timelineTweets = $('<div class="shawnTweets">' + userTweet.message  + '</div>');
-    timelineTweets.appendTo( $( '.shawn-tweet-container' ) );
-    i -= 1;
-  }
-});
+//$(".shawndrost").on('click', function(elem){ //when shawn is clicked on- his tweets are appended to his homepage
+
+  //we want the same tweets from the home page to go into shawns timeline on his homepage                      
+  // var i = streams.users.shawndrost.length - 1;
+  
+  // while(i >= 0){
+  //   var userTweet = streams.users.shawndrost[i];
+  //   var timelineTweets = $('<div class="shawnTweets">' + userTweet.message  + timeStamp() + '</div>');
+  //   timelineTweets.appendTo( $( '.shawn-tweet-container' ) );
+  //   i -= 1;
+  // }
+//});
 
    $(".newtweets").on("click", function(){
      var index = streams.home.length - 1;
